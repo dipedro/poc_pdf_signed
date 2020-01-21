ad7c73eb0763be735825e0f3ca07e82721de67f1
Pedro Azevêdo <dipedrozd@hotmail.com>
2020-01-21 01:00:17 -0300

    assinando e mostrando widget no pdf

--- start/routes.js
+++ start/routes.js
@@ -17,47 +17,52 @@
 const Route = use('Route')
 const fs = use('fs')
 
-const HummusRecipe = use('hummus-recipe')
+//const HummusRecipe = use('hummus-recipe')
 
 const SignPDF = use('node-signpdf')
-const {plainAddPlaceholder} = use('node-signpdf/dist/helpers')
+const {plainAddPlaceholder, pdflibAddPlaceholder} = use('node-signpdf/dist/helpers')
 
 //import para a biblioteca de juntar pdfs em um só.
-const mergePDF = use('easy-pdf-merge')
+//const mergePDF = use('easy-pdf-merge')
 
 const assinador = new SignPDF.SignPdf();
 
-Route.get('/', () => {
+Route.get('/', async () => {
 
-  const pdfDoc = new HummusRecipe('P:/assinatura/assinar.pdf', 'p:/assinatura/output.pdf');
-  pdfDoc
+  //const pdfDoc = new HummusRecipe('D:/arquivo/assinar.pdf', 'D:/arquivo/output.pdf');
+  //pdfDoc
       // edit 1st page
-      .editPage(1)
-      //.text('Pedro assinou essa porra!', 250, 700, {color: '#000000'})
-      .image('P:/assinatura/assinatura.jpg', 200, 650, {width: 250, keepAspectRatio: true})
-      .endPage()
+      //.editPage(1)
+      //.text('Pedro assinou essa porra!', 200, 500, {color: '#000000'})
+      //.image('D:/arquivo/assinatura.jpg', 200, 650, {width: 250, keepAspectRatio: true})
+      //.endPage()
       // end and save
-      .endPDF();
+     //.endPDF();
 
       /**pdfDoc
         .structure('P:/assinatura/pdf-structure.txt')
         .endPDF(done);**/
 
-  const p12_certificado = fs.readFileSync('P:/assinatura/certificate.p12'); //local do certificado p12
-  let pdfBuffer = fs.readFileSync('p:/assinatura/output.pdf'); //local do pdf que vai ser assinado
+  let infoSignature = {
+    reason: 'Ponto',
+    contactInfo: 'pedro.azevedo@iprev.ma.gov.br',
+    name: 'Pedro da Silva Azevedo',
+    location: 'Coordenadoria de TI - CTI'
+  }
+
+  const p12_certificado = fs.readFileSync('D:/arquivo/certificate.p12'); //local do certificado p12
+  let pdfBuffer = fs.readFileSync('D:/arquivo/teste.pdf'); //local do pdf que vai ser assinado
 
   //adicionando a assinatura no pdf (não adiciona a marcação, é apenas uma assinatura invisível, mas o adobe mostra na área de assinaturas)
-  pdfBuffer = plainAddPlaceholder({
+  let pdfBufferRetorno = await pdflibAddPlaceholder({
     pdfBuffer,
-    reason: 'Assinado por Pedro Azevedo.',
-    local: 'IPREV MA',
+    infoSignature,
     signatureLength: p12_certificado.length
   });
 
-  console.log(SignPDF);
-  const signedPdf = assinador.sign(pdfBuffer, p12_certificado);
+  const signedPdf = assinador.sign(pdfBufferRetorno, p12_certificado);
 
-  const outputPath = 'p:/assinatura/pdf_assinado2.pdf';
+  const outputPath = 'D:/arquivo/pdf_assinado2.pdf';
   fs.writeFileSync(outputPath, signedPdf);
 
   return { resultado: 'Assinou o PDF com sucesso!' }
